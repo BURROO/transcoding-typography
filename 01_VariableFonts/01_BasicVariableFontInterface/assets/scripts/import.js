2ff200e603945ebcd9e6c8ace056c49116fb34a4
Philipp Koller <philipp.kooller@googlemail.com>
2025-10-22 00:47:31 +0200

    update course

--- 01_VariableFonts/01_BasicVariableFontInterface/assets/scripts/import.js
+++
@@ -1,7 +0,0 @@
-// This file has the logic for important any font and create the nescessary
-// Font features with the help of opentype js
-
-
-
-// Load font with Drag and drop
-const preview = document.getElementById("preview")
